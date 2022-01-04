4d2b47b546733846b28029ecefc4c6fef86f3a1d
Mikita Byshniou <nick.byshnev@gmail.com>
2022-01-04 22:02:12 +0300

    first commit

---
+++ src/main/java/by/byshnev/BlogBackendApplication.java
@@ -0,0 +1,13 @@
+package by.byshnev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+
+}
