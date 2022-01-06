0648e2937572e94842cccf0e3d2b8ead15892b30
Mikita Byshniou <nick.byshnev@gmail.com>
2022-01-06 18:56:44 +0300

    implement restArticle() method

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -1,16 +1,21 @@
 package by.byshnev.controllers;
 
+import by.byshnev.models.Article;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
-@RequestMapping(path = "/articles",produces = {"application/json"})
 public class ArticlesRestController {
 
-    @GetMapping()
-    public String showArticles() {
-        return null;
+    @RequestMapping(value = "/articles", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Article restArticle(){
+        Article article = new Article(1, "First", "First article", "Content of article", "M.Byshniou", new Date(), new Date());
+        return article;
     }
 }
--- src/main/java/by/byshnev/models/Article.java
+++ src/main/java/by/byshnev/models/Article.java
@@ -97,5 +97,4 @@ public class Article {
             e.printStackTrace();
         }
     }
-
 }
