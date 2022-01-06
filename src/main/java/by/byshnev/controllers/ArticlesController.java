d738a0bff282aba73662645b2ad1c6750b0023a2
Mikita Byshniou <nick.byshnev@gmail.com>
2022-01-06 17:34:18 +0300

    delete HomeController

--- src/main/java/by/byshnev/controllers/ArticlesController.java
+++ src/main/java/by/byshnev/controllers/ArticlesController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/articles")
+@RequestMapping(path = "/articles",produces = {"application/json"})
 public class ArticlesController {
 
     @GetMapping()
