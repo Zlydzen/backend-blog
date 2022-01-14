743627afdd2dde3c9f3e0b2a1d0a0c779dc60171
Zlydzen <nick.byshnev@gmail.com>
2022-01-15 00:31:02 +0300

    implement getArticle() in ArticleServiceImpl

--- src/main/java/by/byshnev/services/ArticleService.java
+++ src/main/java/by/byshnev/services/ArticleService.java
@@ -1,7 +1,9 @@
 package by.byshnev.services;
 
+import by.byshnev.models.Article;
+
 public interface ArticleService {
 
-
+    Article getArticle();
 
 }
--- src/main/java/by/byshnev/services/ArticleServiceImpl.java
+++ src/main/java/by/byshnev/services/ArticleServiceImpl.java
@@ -1,7 +1,16 @@
 package by.byshnev.services;
 
+import by.byshnev.models.Article;
+import by.byshnev.repositories.ArticleRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ArticleServiceImpl implements ArticleService {
 
+    private ArticleRepository articleRepository;
 
-
+    @Override
+    public Article getArticle() {
+        return null;
+    }
 }
