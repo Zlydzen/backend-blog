be88906a7d753882eb4cfaabdd65a3a43b1a6548
Zlydzen <nick.byshnev@gmail.com>
2022-01-15 00:11:38 +0300

    add ArticleRepository interface

---
+++ src/main/java/by/byshnev/repositories/ArticleRepository.java
@@ -0,0 +1,9 @@
+package by.byshnev.repositories;
+
+import by.byshnev.models.Article;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends CrudRepository<Article, Long> {
+}
