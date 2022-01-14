2026954cc296357617240308a8501a9042847555
Zlydzen <nick.byshnev@gmail.com>
2022-01-14 19:08:02 +0300

    changes in pom and restcontroller

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -1,11 +1,15 @@
 package by.byshnev.controllers;
 
 import by.byshnev.models.Article;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -13,8 +17,12 @@ import java.util.List;
 public class ArticlesRestController {
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Article> restArticle() {
-        return null;
+    public List<Article> restArticle() throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Article article = objectMapper.readValue(new File("/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json"), Article.class);
+        List<Article> articleList = new ArrayList<>();
+        articleList.add(article);
+        return articleList;
     }
 }
 
