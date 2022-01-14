08500b5779187d86d44eb9070ad13cb105fd7c34
Zlydzen <nick.byshnev@gmail.com>
2022-01-14 19:37:45 +0300

    not working check point

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -1,15 +1,17 @@
 package by.byshnev.controllers;
 
 import by.byshnev.models.Article;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -19,9 +21,14 @@ public class ArticlesRestController {
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Article> restArticle() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        Article article = objectMapper.readValue(new File("/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json"), Article.class);
         List<Article> articleList = new ArrayList<>();
-        articleList.add(article);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(
+                "/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json"));
+        while (bufferedReader.ready()) {
+            String jsonArticleArray = bufferedReader.readLine();
+            articleList = objectMapper.readValue(jsonArticleArray, new TypeReference<>() {
+            });
+        }
         return articleList;
     }
 }
