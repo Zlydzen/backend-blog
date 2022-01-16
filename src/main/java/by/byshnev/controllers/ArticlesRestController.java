051b7f9b0860440e17023677ff8201db6589a678
Zlydzen <nick.byshnev@gmail.com>
2022-01-16 18:49:30 +0300

    implement getAllArticles in ArticleService

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -1,38 +1,33 @@
 package by.byshnev.controllers;
 
 import by.byshnev.dto.ArticleDto;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/articles")
 public class ArticlesRestController {
 
-    @Value("/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json")
-    String getPATH;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleDto> getAllArticles() {
-        List<ArticleDto> articleList = null;
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
-            InputStream inputStream = new FileInputStream(new File(getPATH));
-            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
-            articleList = objectMapper.readValue(inputStream, typeReference);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return articleList;
+        return null;
     }
 }
+
+//        public List<ArticleDto> getAllArticles() {
+//        try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
+//            InputStream inputStream = new FileInputStream(new File(getPATH));
+//            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
+//            articleList = objectMapper.readValue(inputStream, typeReference);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return articleList;
 
--- src/main/java/by/byshnev/services/ArticleService.java
+++ src/main/java/by/byshnev/services/ArticleService.java
@@ -1,6 +1,44 @@
 package by.byshnev.services;
 
+import by.byshnev.dto.ArticleDto;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class ArticleService {
 
+    @Value("articles.json")
+    Resource getFile;
 
+    public List<ArticleDto> getAllArticles() {
+        ObjectMapper mapper = new ObjectMapper();
+        List<ArticleDto> list = new ArrayList<>();
+        TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
+        try {
+            list = mapper.readValue((JsonParser) getFile, typeReference);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
+
+//            try {
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
+//            InputStream inputStream = new FileInputStream(new File(getPATH));
+//            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
+//            articleList = objectMapper.readValue(inputStream, typeReference);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return articleList;
+
