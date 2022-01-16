16481147105c8819048be13c1384eb861730ee99
Zlydzen <nick.byshnev@gmail.com>
2022-01-16 16:41:41 +0300

    incapsulate path using Value annotation

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -3,6 +3,7 @@ package by.byshnev.controllers;
 import by.byshnev.dto.ArticleDto;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,7 +16,8 @@ import java.util.List;
 @RequestMapping("/articles")
 public class ArticlesRestController {
 
-    private final String PATH = "/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json";
+    @Value("/Users/Nikitos/Desktop/backend-blog/src/main/resources/articles.json")
+    String getPATH;
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleDto> getAllArticles() {
@@ -23,7 +25,7 @@ public class ArticlesRestController {
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
-            InputStream inputStream = new FileInputStream(new File(PATH));
+            InputStream inputStream = new FileInputStream(getPATH);
             TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
             articleList = objectMapper.readValue(inputStream,typeReference);
         } catch (IOException e) {
