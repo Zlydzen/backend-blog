75f003d8f66ca38ccf3289f1c949032b1f9e5a0c
Zlydzen <nick.byshnev@gmail.com>
2022-01-16 20:15:02 +0300

    implement getAllArticles

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -1,6 +1,7 @@
 package by.byshnev.controllers;
 
 import by.byshnev.dto.ArticleDto;
+import by.byshnev.services.ArticleService;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,22 +13,14 @@ import java.util.List;
 @RequestMapping("/articles")
 public class ArticlesRestController {
 
+    private final ArticleService articleService;
+
+    public ArticlesRestController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleDto> getAllArticles() {
-        return null;
+        return articleService.getAllArticles();
     }
 }
-
-//        public List<ArticleDto> getAllArticles() {
-//        try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
-//            InputStream inputStream = new FileInputStream(new File(getPATH));
-//            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
-//            articleList = objectMapper.readValue(inputStream, typeReference);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return articleList;
-
--- src/main/java/by/byshnev/services/ArticleService.java
+++ src/main/java/by/byshnev/services/ArticleService.java
@@ -1,7 +1,6 @@
 package by.byshnev.services;
 
 import by.byshnev.dto.ArticleDto;
-import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,36 +8,28 @@ import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ArticleService {
 
-    @Value("articles.json")
+    @Value("classpath:articles.json")
     Resource getFile;
 
     public List<ArticleDto> getAllArticles() {
         ObjectMapper mapper = new ObjectMapper();
         List<ArticleDto> list = new ArrayList<>();
-        TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
         try {
-            list = mapper.readValue((JsonParser) getFile, typeReference);
+            InputStream inputStream = getFile.getInputStream();
+            mapper.findAndRegisterModules(); // without it -> InvalidDefinitionException: Java 8 date/time type `java.time.LocalDate` not supported by default: add Module "com.fasterxml.jackson.datatype:jackson-datatype-jsr310" to enable handling
+            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
+            list = mapper.readValue(inputStream, typeReference);
         } catch (IOException e) {
             e.printStackTrace();
+            System.out.println("File not found");
         }
         return list;
     }
 }
-
-//            try {
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            objectMapper.findAndRegisterModules();  // Java 8 date/time type `java.time.LocalDate` not supported by default
-//            InputStream inputStream = new FileInputStream(new File(getPATH));
-//            TypeReference<List<ArticleDto>> typeReference = new TypeReference<List<ArticleDto>>() {};
-//            articleList = objectMapper.readValue(inputStream, typeReference);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return articleList;
-
