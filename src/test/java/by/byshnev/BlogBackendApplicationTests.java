2d726bb1ddce7183107b47ae8f4fb984e1a3dac6
Zlydzen <nick.byshnev@gmail.com>
2022-01-17 12:15:55 +0300

    implement my first test

--- src/test/java/by/byshnev/BlogBackendApplicationTests.java
+++ src/test/java/by/byshnev/BlogBackendApplicationTests.java
@@ -1,13 +1,20 @@
 package by.byshnev;
 
+import by.byshnev.controllers.ArticlesRestController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class BlogBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private ArticlesRestController articlesRestController;
+
+    @Test
+    void contextLoads() throws Exception {
+        assertThat(articlesRestController).isNotNull();
+    }
 
 }
