9a1862a7918edfe4b887f3b7cf3f5282e28f97f7
Zlydzen <nick.byshnev@gmail.com>
2022-01-19 16:45:33 +0300

    implement mapToArticle in MapperUtil

--- src/main/java/by/byshnev/services/MapperUtil.java
+++ src/main/java/by/byshnev/services/MapperUtil.java
@@ -9,13 +9,26 @@ public class MapperUtil {
 
     public ArticleDto mapToArticleDto(Article article) {
         ArticleDto dto = new ArticleDto();
-    dto.setId(article.getId());
-    dto.setTitle(article.getTitle());
-    dto.setShortText(article.getShortText());
-    dto.setMainText(article.getMainText());
-    dto.setAuthor(article.getAuthor());
-    dto.setCreationDateTime(article.getCreationDateTime());
-    dto.setUpdateDateTime(article.getUpdateDateTime());
-    return dto;
+        dto.setId(article.getId());
+        dto.setTitle(article.getTitle());
+        dto.setShortText(article.getShortText());
+        dto.setMainText(article.getMainText());
+        dto.setAuthor(article.getAuthor());
+        dto.setCreationDateTime(article.getCreationDateTime());
+        dto.setUpdateDateTime(article.getUpdateDateTime());
+        return dto;
     }
+
+    public Article mapToArticle(ArticleDto dto) {
+        Article article = new Article();
+        article.setId(dto.getId());
+        article.setTitle(dto.getTitle());
+        article.setShortText(dto.getShortText());
+        article.setMainText(dto.getMainText());
+        article.setAuthor(dto.getAuthor());
+        article.setCreationDateTime(dto.getCreationDateTime());
+        article.setUpdateDateTime(dto.getUpdateDateTime());
+        return article;
+    }
+
 }
