a9e76a4885dfc7570fa1946b589e13402a40c7f5
Zlydzen <nick.byshnev@gmail.com>
2022-01-21 09:08:22 +0300

    remove annotation 'Service' in ArticleDAO interface

--- src/main/java/by/byshnev/dao/ArticleDAO.java
+++ src/main/java/by/byshnev/dao/ArticleDAO.java
@@ -1,11 +1,8 @@
 package by.byshnev.dao;
 
 import by.byshnev.dto.ArticleDto;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
 public interface ArticleDAO {
     List<ArticleDto> allArticles();
 }
