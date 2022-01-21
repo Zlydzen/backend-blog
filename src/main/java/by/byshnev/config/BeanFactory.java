d74f54907fd43856477056c9db694e23f43e4671
Zlydzen <nick.byshnev@gmail.com>
2022-01-21 10:25:21 +0300

    create new bean 'objectMapper' in BeanFactory

--- src/main/java/by/byshnev/config/BeanFactory.java
+++ src/main/java/by/byshnev/config/BeanFactory.java
@@ -1,7 +1,15 @@
 package by.byshnev.config;
 
+import by.byshnev.services.Mapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanFactory {
+
+    @Bean
+    public ObjectMapper objectMapper(){
+        return new ObjectMapper();
+    }
 }
