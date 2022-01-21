7b0a65cc8e215c4a42abf1c953b7d88b58ed8c94
Zlydzen <nick.byshnev@gmail.com>
2022-01-21 10:37:02 +0300

    clean some spaces in code

--- src/main/java/by/byshnev/config/BeanFactory.java
+++ src/main/java/by/byshnev/config/BeanFactory.java
@@ -1,6 +1,5 @@
 package by.byshnev.config;
 
-import by.byshnev.services.Mapper;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
