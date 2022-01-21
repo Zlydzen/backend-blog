ce11c99243ef54249555e906432fa9e7a640c5f9
Zlydzen <nick.byshnev@gmail.com>
2022-01-22 00:47:57 +0300

    update Unit test

--- src/test/java/by/byshnev/BlogBackendApplicationTests.java
+++ src/test/java/by/byshnev/BlogBackendApplicationTests.java
@@ -1,16 +1,24 @@
 package by.byshnev;
 
 import by.byshnev.controllers.ArticlesRestController;
+import by.byshnev.dto.ArticleDto;
+import by.byshnev.models.ArticleEntity;
+import by.byshnev.services.ArticleService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -19,15 +27,22 @@ class BlogBackendApplicationTests {
     private MockMvc mockMvc;
 
     @Autowired
+    private ArticleService articleService;
+
+    @Autowired
     private ArticlesRestController articlesRestController;
 
     @Test
     void contextLoads() throws Exception {
         assertThat(articlesRestController).isNotNull();
 
-        this.mockMvc.perform(get("/articles"))
+        MvcResult mvcResult = this.mockMvc.perform(get("/articles"))
                 .andDo(print())
-                .andExpect(status().isOk());
-    }
+                .andExpect(status().isOk())
+                .andReturn();
 
+        String contentJsonAsString = mvcResult.getResponse().getContentAsString();
+        System.out.println(contentJsonAsString);
+
+    }
 }
