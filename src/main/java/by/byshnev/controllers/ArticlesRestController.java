be4aea50e91b8c4aa497ac6612cd46030f09fddb
Zlydzen <nick.byshnev@gmail.com>
2022-01-31 15:19:17 +0300

    first exception handling in controller

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -3,8 +3,10 @@ package by.byshnev.controllers;
 import by.byshnev.dto.ArticleDto;
 import by.byshnev.services.ArticleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -21,7 +23,12 @@ public class ArticlesRestController {
     }
 
     @GetMapping(value = "/{id}")
-    public ArticleDto getById(@PathVariable("id") int id){
-        return articleService.getById(id);
+    public ArticleDto getById(@PathVariable("id") int id) {
+        try {
+            return articleService.getById(id);
+        } catch (IndexOutOfBoundsException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.NOT_FOUND);
+        }
     }
 }
