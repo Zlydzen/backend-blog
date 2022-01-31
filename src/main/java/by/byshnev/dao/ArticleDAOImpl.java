e5840aae6c7e1a7fb3fc1b48f6defc8b9d6dec11
Zlydzen <nick.byshnev@gmail.com>
2022-01-31 14:50:07 +0300

    simplify ArticleDAOImple

--- src/main/java/by/byshnev/dao/ArticleDAOImpl.java
+++ src/main/java/by/byshnev/dao/ArticleDAOImpl.java
@@ -38,13 +38,7 @@ public class ArticleDAOImpl implements ArticleDAO {
 
     @Override
     public Article getById(int id) {
-        Article article = null;
-        try {
-            article = allArticles().get(id);
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("there is no article with this id");
-        }
-        return article;
+        return allArticles().get(id);
     }
 }
 
