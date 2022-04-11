d35f60c91db672fe48f3cbdd26d592992cb79069
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-12 00:10:38 +0300

    code clean, add test

--- src/test/java/by/byshnev/BlogBackendApplicationTests.java
+++ src/test/java/by/byshnev/BlogBackendApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -41,6 +40,5 @@ class BlogBackendApplicationTests {
 
         String contentJsonAsString = mvcResult.getResponse().getContentAsString();
         System.out.println(contentJsonAsString);
-
     }
 }
