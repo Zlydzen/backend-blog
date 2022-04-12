24b4fee2c400253489732a4dad96eea0bdcc874e
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-12 16:35:13 +0300

    use articleRepo against dao

--- src/main/java/by/byshnev/services/ArticleServiceImpl.java
+++ src/main/java/by/byshnev/services/ArticleServiceImpl.java
@@ -4,6 +4,7 @@ import by.byshnev.dao.ArticleDAO;
 import by.byshnev.dto.ArticleDto;
 import by.byshnev.entities.Article;
 import by.byshnev.mappers.ArticleMapper;
+import by.byshnev.repo.ArticleRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,9 +18,11 @@ public class ArticleServiceImpl implements ArticleService {
 
     private final ArticleDAO articleDAO;
 
+    private ArticleRepo articleRepo;
+
     @Override
     public List<ArticleDto> getAllArticles() {
-        List<Article> articles = articleDAO.allArticles();
+        List<Article> articles = articleRepo.findAll();
         return articleMapper.toListArticleDto(articles);
     }
 
