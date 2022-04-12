368bb3a0609de47425ca8aae2d100aea728c66ec
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-12 11:50:07 +0300

    update addArticle in RestController

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class ArticlesRestController {
 
     private final ArticleService articleService;
+    private int counter = articleService.getAllArticles().size();
 
     @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<ArticleDto> getAllArticles() {
@@ -21,9 +22,8 @@ public class ArticlesRestController {
     }
 
     @PostMapping("/new")
-    public ArticleDto addArticle(@RequestBody ArticleDto articleDto){
-        articleDto.setId(0);
+    public void addArticle(@RequestBody ArticleDto articleDto){
+        articleDto.setId(counter++);
         articleService.addArticleDto(articleDto);
-        return articleDto;
     }
 }
