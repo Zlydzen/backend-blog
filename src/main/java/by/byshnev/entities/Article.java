ba69288b25f663f08a8dd417d52a5a33c4ea70cb
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-12 16:14:14 +0300

    add 'table' annotation into entity (Article.class)

--- src/main/java/by/byshnev/entities/Article.java
+++ src/main/java/by/byshnev/entities/Article.java
@@ -2,14 +2,12 @@ package by.byshnev.entities;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@Table
 public class Article {
 
     @Id
