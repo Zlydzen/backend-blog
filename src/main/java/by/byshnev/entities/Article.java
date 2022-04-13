4cd6997558c473b08b768d26c8b35b67d461872f
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-13 11:03:26 +0300

    add annotaions into entity

--- src/main/java/by/byshnev/entities/Article.java
+++ src/main/java/by/byshnev/entities/Article.java
@@ -7,16 +7,29 @@ import java.time.LocalDate;
 
 @Entity
 @Data
-@Table
+@Table(name = "article")
 public class Article {
 
     @Id
+    @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name = "title")
     private String title;
+
+    @Column(name = "shorttext")
     private String shortText;
+
+    @Column(name = "maintext")
     private String mainText;
+
+    @Column(name = "author")
     private String author;
+
+    @Column(name = "creationdatetime")
     private LocalDate creationDateTime;
+
+    @Column(name = "updatedatetime")
     private LocalDate updateDateTime;
 }
