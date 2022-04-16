7a9d5441362c94fe5003cd1a1303a49aca77a97a
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-16 18:01:12 +0300

    add important annotation - mean reason why my app doesn't work

--- src/main/java/by/byshnev/repo/ArticleRepo.java
+++ src/main/java/by/byshnev/repo/ArticleRepo.java
@@ -2,6 +2,8 @@ package by.byshnev.repo;
 
 import by.byshnev.entities.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ArticleRepo extends JpaRepository<Article, Integer> {
 }
