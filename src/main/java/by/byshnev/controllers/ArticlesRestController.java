bcd1f93db0671626032102d9b8ded56919f05614
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-16 17:29:35 +0300

    add not found status in getAll method

--- src/main/java/by/byshnev/controllers/ArticlesRestController.java
+++ src/main/java/by/byshnev/controllers/ArticlesRestController.java
@@ -19,16 +19,18 @@ public class ArticlesRestController {
     @GetMapping
     public ResponseEntity<List<ArticleDto>> getAll() {
         List<ArticleDto> allArticles = articleService.getAllArticles();
-        return new ResponseEntity<>(allArticles, HttpStatus.OK);
+        return allArticles != null && !allArticles.isEmpty()
+                ? new ResponseEntity<>(allArticles, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("/new")
+    @PostMapping(value = "/new")
     public ResponseEntity<?> createArticle(@RequestBody ArticleDto articleDto) {
         articleService.addOne(articleDto);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<ArticleDto> getOne(@PathVariable(name = "id") int id) {
         ArticleDto byId = articleService.getById(id);
         return byId != null ? new ResponseEntity<>(byId, HttpStatus.OK) : new ResponseEntity<>(HttpStatus.NOT_FOUND);
