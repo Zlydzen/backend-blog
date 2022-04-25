a47a0c84b525719a626e13889eb9659da9377540
Mikita Byshniou <nick.byshnev@gmail.com>
2022-04-25 11:12:28 +0300

    add update methid into service interface

--- src/main/java/by/byshnev/services/ArticleService.java
+++ src/main/java/by/byshnev/services/ArticleService.java
@@ -9,4 +9,5 @@ public interface ArticleService {
     List<ArticleDto> getArticles();
     Optional<ArticleDto> getById(int id);
     void create(ArticleDto articleDto);
+    ArticleDto update(ArticleDto articleDto, int id);
 }
